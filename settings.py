4c14c968a8abd5457b81992eda098d507dadf24f
Teng Wei <94235563+tw-ngwan@users.noreply.github.com>
2022-10-16 23:45:24 +0800

    Updated ytdownloader.py with pytube functionality

---
+++ settings.py
@@ -0,0 +1,9 @@
+
+ytdownloader_usage = """
+Usage:
+There will be two possible functionalities for this code. 
+First, key in all links under the input; the downloader will download all of them. 
+Second, key in all links into a text document, and key in the document name. All links inside will be downloaded. 
+The first is that you key in a few links under input, and the downloader downloads
+Key in "py ytdownloader.py a" for links, "py ytdownloader.py t <.txt>" for text
+"""
--- ytdownloader.py
+++ ytdownloader.py
@@ -1,93 +1,64 @@
-# from lxml import html
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import re, time, sys, webbrowser
-from bs4 import BeautifulSoup
-
-usage = """
-Usage: 
-There will be two possible functionalities for this code. The first is that you key in a few links under input, and the downloader downloads
-them all. The second is that you key in a text document, it parses the document for links, and downloads every link in the text document. 
-Key in "py ytdownloader.py a for links, py ytdownloader.py t <.txt> for text"
-"""
+from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+import sys
+from settings import ytdownloader_usage
 
 
-def main():
+# The main function in ytdownloader that gets user input and downloads all videos
+def get_all_videos():
     if len(sys.argv) < 2:
-        print(usage)
+        print(ytdownloader_usage)
+        sys.exit()
     elif sys.argv[1].lower() == 'a' and len(sys.argv) == 2:
-        links_parse()
+        video_links = links_parse()
     elif sys.argv[1].lower() == 't' and len(sys.argv) == 3:
-        text_parse()
+        video_links = text_parse()
     else:
         print("No such function!")
-        print(usage)
+        print(ytdownloader_usage)
         sys.exit()
+    download_all_videos(video_links)
 
 
-def text_parse():
+# Parses through the text document to find all links to download
+def text_parse() -> list[str]:
     print("Opening the text document to extract links...")
     try:
         with open(sys.argv[2], 'r') as f:
             contents = f.read()
             print(contents)
-            links = contents.split('\n')
+            links = contents.split()
             print("Added all links")
-            download_videos(links)
-    except:
+            return links
+    except FileNotFoundError:
         print("Can't open document!")
 
 
+# Gets all links to be downloaded
 def links_parse():
     links_string = input("Copy the links you want to download, separated by a space \n")
     links = links_string.split()
     print("Added all links")
-    print(links)
-    download_videos(links)
+    return links
 
 
-PATH = 'C:\\Users\Tengwei\Desktop\PythonStuff\chromedriver.exe'
+# Downloads all videos with their urls
+def download_all_videos(urls: list[str], outpath: str="./"):
+    for url in urls:
+        download_video(url, outpath)
+    print("All videos downloaded")
 
 
-def download_videos(links):
-    linkRegex = re.compile(r"(https://www\.)(.*)")
-    print("Compiling links...")
-    for link in links:
-        while True:
-            link_parts = linkRegex.findall(link)
-            print("Obtaining download link...")
-            download_link = link_parts[0][0] + 'ss' + link_parts[0][1]
-            print(download_link)
-            driver = webdriver.Chrome(PATH)
-            driver.get(download_link)
-            try:
-                elem = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.XPATH, '//*[@id="sf_result"]/div/div[1]/div[2]/div[2]/div[1]/a'))
-                )
-                break
-            except:
-                print("Trying again 1...")
-
-        time.sleep(1)
-        soup_file = driver.page_source
-        soup = BeautifulSoup(soup_file, features='lxml')
-        video_downloader = soup.find('a', {"data-type": "mp4"})
-        new_driver = webdriver.Chrome(PATH)
-        new_driver.get(str(video_downloader.get('href')))
-        # webbrowser.open(str(video_downloader.get('href'))) # Try and replace this with some webdriver. It's too damn slow in webbrowser
-
-        print("Video downloading...")
-
-    input("Press any key to conclude the downloads \n")
-    # driver.close()
-    print("All videos downloaded.")
+# Downloads a video with the url
+def download_video(url: str, outpath: str="./"):
+    yt = YouTube(url)
+    try:
+        yt.streams.filter(file_extension="mp4").get_by_resolution("240p").download(outpath)
+    except RegexMatchError:
+        print(f"{url} video not found!")
+        return
+    print(f"{url} downloaded")
 
 
 if __name__ == "__main__":
-    main()
-
-# You may want to use BeautifulSoup to extract the href link from the Download button, instead of clicking it
-# This ensures that you won't be redirected to another page for other stupid reasons.
-# So this means extracting the href link, then opening it with selenium.webdriver again?
+    get_all_videos()
